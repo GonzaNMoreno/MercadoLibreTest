c505128ffeb26ee019b1d88508d33df1bf2a76fe
Gonza <gonzaamoreno@icloud.com>
2017-01-12 01:13:58 -0300

    Iniciando cucumber

---
+++ src/test/java/com/bootcamp/cucumber/TestRunner.java
@@ -0,0 +1,7 @@
+package com.bootcamp.cucumber;
+
+/**
+ * Created by GonzaMoreno on 12/1/17.
+ */
+public class TestRunner {
+}
---
+++ src/test/java/com/bootcamp/cucumber/definitions/ItemSearchSteps.java
@@ -0,0 +1,7 @@
+package com.bootcamp.cucumber.definitions;
+
+/**
+ * Created by GonzaMoreno on 12/1/17.
+ */
+public class ItemSearchSteps {
+}
