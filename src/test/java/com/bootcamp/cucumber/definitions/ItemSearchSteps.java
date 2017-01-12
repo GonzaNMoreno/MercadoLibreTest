8208ee7df0d4b97789d1157b4ee863eec301afb0
Gonza <gonzaamoreno@icloud.com>
2017-01-12 14:19:56 -0300

    Test Runner and ItemSearchSteps Added

--- src/test/java/com/bootcamp/cucumber/definitions/ItemSearchSteps.java
+++ src/test/java/com/bootcamp/cucumber/definitions/ItemSearchSteps.java
@@ -1,7 +1,30 @@
 package com.bootcamp.cucumber.definitions;
 
+import com.bootcamp.framework.runner.GenericWebAutomationTest;
+import com.bootcamp.framework.web.Browser;
+import com.bootcamp.pageobjects.MercadoLibreHome;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+import java.util.Optional;
+
 /**
  * Created by GonzaMoreno on 12/1/17.
  */
-public class ItemSearchSteps {
+public class ItemSearchSteps extends GenericWebAutomationTest<MercadoLibreHome>{
+
+    private;
+    private;
+
+    @Before
+    public void setUp(){
+        super.setUp(Browser.valueOf(Optional.ofNullable(System.getenv("CUCUMBER_BROWSER")).orElse("CHROME")));
+    }
+
+    @After
+    public void tearDown(){
+        super.tearDown();
+    }
+
+
 }
